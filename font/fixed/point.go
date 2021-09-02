208d510e7fbc34b680e35c8ad08749bab6423af7
Sebastien Binet <s@sbinet.org>
2021-09-02 11:56:10 +0200

    font/fixed: introduce Point12_20
    Signed-off-by: Sebastien Binet <s@sbinet.org>

---
+++ font/fixed/point.go
@@ -0,0 +1,12 @@
+// Copyright ©2021 The star-tex Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fixed
+
+// Point12_20 is a 12.20 fixed-point coordinate pair.
+//
+// It is analogous to the image.Point type in the standard library.
+type Point12_20 struct {
+	X, Y Int12_20
+}
